812ce89ecb2946f855347ae8045cdbcf20ee5586
Mohammad Hedayati <hedayati@cs.rochester.edu>
2018-10-27 20:30:20 -0400

    fix compilation issue with gcc 8

--- linux-4.15/tools/lib/str_error_r.c
+++ linux-4.15/tools/lib/str_error_r.c
@@ -21,7 +21,12 @@
 char *str_error_r(int errnum, char *buf, size_t buflen)
 {
 	int err = strerror_r(errnum, buf, buflen);
-	if (err)
-		snprintf(buf, buflen, "INTERNAL ERROR: strerror_r(%d, %p, %zd)=%d", errnum, buf, buflen, err);
+	if (err) {
+		char *err_buf = buf;
+
+		snprintf(err_buf, buflen,
+			 "INTERNAL ERROR: strerror_r(%d, %p, %zd)=%d",
+			 errnum, buf, buflen, err);
+	}
 	return buf;
 }
